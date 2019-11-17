dd26edf71a04d03a1ca8a8abe75ad4d648fb335f
codeaffect <sandeepjhankar@gmail.com>
2019-11-16 22:34:52 -0600

    stringending

--- strings/manipulations.py
+++ strings/manipulations.py
@@ -1,16 +1,22 @@
-print('*'*10+'START'+'*'*10)
-message = 'How are you'
-#index of a string
-print (f'Index of "you" in "{message}"')
-print(message.find('you'))
-#replace string
-print('Replacing "you" with "YOU"');
-message = message.replace('you','YOU')
+print("*" * 10 + "START" + "*" * 10)
+message = "How are you"
+# index of a string
+print(f'Index of "you" in "{message}"')
+print(message.find("you"))
+# replace string
+print('Replacing "you" with "YOU"')
+message = message.replace("you", "YOU")
 print(message)
-#lower and upper case
-print('lower:'+message.lower()+'\nUPPER:'+message.upper())
-#checking values in a string
+# lower and upper case
+print("lower:" + message.lower() + "\nUPPER:" + message.upper())
+# checking values in a string
 print(f'Does "are" exists in "{message}"?')
-print('are' in message)
-#title()
-print('title()::--',message.title())
+print("are" in message)
+# title()
+print("title()::--", message.title())
+# ending commas
+counter = 1
+while counter < 10:
+    print(counter, end=",")
+    counter += 1
+
